88db9c502f8baffdb15b7be9c1efa1374491e172
kphed <kpcrypto69@gmail.com>
2023-11-03 08:55:26 -0400

    Add setBaseTokenURI method

--- src/Rumas.sol
+++ src/Rumas.sol
@@ -3,13 +3,28 @@ pragma solidity ^0.8.0;
 
 import {ERC721} from "solmate/tokens/ERC721.sol";
 import {LibString} from "solmate/utils/LibString.sol";
+import {Owned} from "solmate/auth/Owned.sol";
 
-contract BaseRumas is ERC721("BRR Rumas", "RUMAS") {
+contract Rumas is Owned, ERC721("BRR Rumas", "RUMAS") {
     using LibString for uint256;
 
-    string private _baseTokenURI = "https://egap.j.page/rumas/metadata/";
+    string public baseTokenURI = "https://egap.j.page/rumas/metadata/";
+
+    event SetBaseTokenURI(string);
+
+    error InvalidNFT();
+
+    constructor(address _owner) Owned(_owner) {}
 
     function tokenURI(uint256 id) public view override returns (string memory) {
-        return string.concat(_baseTokenURI, id.toString());
+        if (_ownerOf[id] == address(0)) revert InvalidNFT();
+
+        return string.concat(baseTokenURI, id.toString());
+    }
+
+    function setBaseTokenURI(string calldata _baseTokenURI) external onlyOwner {
+        baseTokenURI = _baseTokenURI;
+
+        emit SetBaseTokenURI(_baseTokenURI);
     }
 }
--- test/Rumas.t.sol
+++ test/Rumas.t.sol
@@ -2,6 +2,58 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Test.sol";
+import {LibString} from "solmate/utils/LibString.sol";
 import {Rumas} from "src/Rumas.sol";
 
-contract RumasTest is Test {}
+contract RumasTest is Test {
+    using LibString for uint256;
+
+    Rumas public immutable rumas;
+
+    constructor() {
+        rumas = new Rumas(address(this));
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                             setBaseTokenURI
+    //////////////////////////////////////////////////////////////*/
+
+    function testCannotSetBaseTokenURIUnauthorized() external {
+        address msgSender = address(0);
+        string memory baseTokenURI = "https://brrito.xyz/";
+
+        assertTrue(msgSender != rumas.owner());
+
+        vm.prank(msgSender);
+        vm.expectRevert("UNAUTHORIZED");
+
+        rumas.setBaseTokenURI(baseTokenURI);
+    }
+
+    function testSetBaseTokenURI() external {
+        address msgSender = rumas.owner();
+        string memory baseTokenURI = "https://brrito.xyz/";
+
+        vm.prank(msgSender);
+        vm.expectEmit(false, false, false, true, address(rumas));
+
+        emit Rumas.SetBaseTokenURI(baseTokenURI);
+
+        rumas.setBaseTokenURI(baseTokenURI);
+
+        assertEq(baseTokenURI, rumas.baseTokenURI());
+    }
+
+    function testSetBaseTokenURIFuzz(string calldata baseTokenURI) external {
+        address msgSender = rumas.owner();
+
+        vm.prank(msgSender);
+        vm.expectEmit(false, false, false, true, address(rumas));
+
+        emit Rumas.SetBaseTokenURI(baseTokenURI);
+
+        rumas.setBaseTokenURI(baseTokenURI);
+
+        assertEq(baseTokenURI, rumas.baseTokenURI());
+    }
+}

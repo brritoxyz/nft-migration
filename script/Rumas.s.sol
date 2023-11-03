958c0f9b7e2ae0d42e692d60e71bded7ceb7c29c
kphed <kpcrypto69@gmail.com>
2023-11-03 09:44:57 -0400

    Update the Rumas deployment script

--- script/Rumas.s.sol
+++ script/Rumas.s.sol
@@ -2,11 +2,30 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
+import {Rumas} from "src/Rumas.sol";
 
 contract RumasScript is Script {
-    function setUp() public {}
+    function _getAirdropData()
+        internal
+        view
+        returns (address[] memory recipients, uint256[] memory tokenIds)
+    {
+        // Rumas owners and token IDs at the time of the post-mint snapshot:
+        // https://github.com/brritoxyz/token-distribution/blob/main/src/generateRumasAirdrop.js.
+        string memory file = vm.readFile("script/airdropData/index.json");
+
+        recipients = vm.parseJsonAddressArray(file, ".initialTokenRecipients");
+        tokenIds = vm.parseJsonUintArray(file, ".initialTokenIds");
+    }
 
     function run() public {
-        vm.broadcast();
+        (
+            address[] memory recipients,
+            uint256[] memory tokenIds
+        ) = _getAirdropData();
+
+        vm.broadcast(vm.envUint("PRIVATE_KEY"));
+
+        new Rumas(vm.envAddress("OWNER"), recipients, tokenIds);
     }
 }
